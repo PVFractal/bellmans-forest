f60ce04b73778d578f31cbbe2cb11b2e60b4e50e
Peter Vahlberg <peter@vahlberg.org>
2025-05-05 12:07:41 -0700

    Removed console.logs

--- project/index.js
+++ project/index.js
@@ -100,11 +100,6 @@ function mouseClick(event) {
   } else {
     let newLine = new Line(lastClickX, lastClickY, x, y);
 
-    console.log(newLine.x1);
-    console.log(newLine.y1);
-    console.log(newLine.x2);
-    console.log(newLine.y2);
-
     let didCollide = collisionCheck(newLine);
 
     if (didCollide === false) {
